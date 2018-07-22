cd6a5e55b416faf341a3ee901db2e781622c226d
aveia <46282133+aveia@users.noreply.github.com>
2018-07-21 21:20:28 -0400

    bump version to v0.2

--- get-site.py
+++ get-site.py
@@ -142,7 +142,7 @@ class SiteDownloader:
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser('get site v0.1')
+    parser = argparse.ArgumentParser('get site v0.2')
     parser.add_argument('-s', required=True, dest='url')
     parser.add_argument('-o', required=False, dest='out', default='tmpsite')
 
